9fa4b55c0a29ea4f9367329da39f4f700cf4c496
Sydney K <73449850+knauerL@users.noreply.github.com>
2021-12-07 17:34:28 -0500

    Chapter 1 Commit

---
+++ gitvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\users\sydst\onedrive\documents\github\python\gitvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
