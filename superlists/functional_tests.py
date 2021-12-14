9b8b4989bf0920fa9c9c263af804585358e8097c
Sydney K <73449850+knauerL@users.noreply.github.com>
2021-12-14 16:07:56 -0500

    chapter 5 git commit - changes to home/tests/functional_tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,9 @@
-from typing import KeysView
-from selenium import webdriver
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
+from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
 class NewVisitorTest(unittest.TestCase) :
     def setUp(self):
@@ -34,16 +36,16 @@ class NewVisitorTest(unittest.TestCase) :
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(KeysView.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows]
         )
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
         # She types "Buy peacock feathers" into a text box 
