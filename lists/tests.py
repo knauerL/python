dff7d4650b2a71c7cc4e10503b6107a85a804439
Sydney K <73449850+knauerL@users.noreply.github.com>
2021-12-14 19:26:53 -0500

    ch7 changed files and migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
+from typing import List
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest, response
+import lists
 from lists.views import home_page
 from lists.models import Item
 
@@ -20,13 +22,21 @@ class HomePageTest(TestCase):
         self.assertContains(response, 'itemey 2') 
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = lists.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -34,7 +44,9 @@ class HomePageTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -45,13 +57,9 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
