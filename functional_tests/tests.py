49da7ad9fb92e2c237fece4b47dac4013916520d
Sydney K <73449850+knauerL@users.noreply.github.com>
2021-12-21 19:03:58 -0500

    hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,3 @@
-import unittest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -19,7 +18,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -32,7 +30,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -72,7 +69,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Satisfied, she goes back to sleep
-
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -142,6 +138,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta=10
         )
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
