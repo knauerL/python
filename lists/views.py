140b905bee621bfe5d8d5b44168f47da8bf9a070
Sydney K <73449850+knauerL@users.noreply.github.com>
2022-01-06 21:36:54 -0500

    end of ch14 commit

--- lists/views.py
+++ lists/views.py
@@ -8,16 +8,13 @@ def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -30,6 +27,6 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
