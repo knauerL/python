cd364b121ea07495f21f5a2ee0567a2a5b098868
Sydney K <73449850+knauerL@users.noreply.github.com>
2022-01-06 22:00:59 -0500

    end of ch15 commit

--- lists/forms.py
+++ lists/forms.py
@@ -16,6 +16,5 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
         
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
+    def save(self):
+        return forms.models.ModelForm.save(self)
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -10,6 +10,12 @@ DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemFormTest(TestCase):
 
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
+
     def test_form_renders_item_text_input(self):
         form = ItemForm()
         self.fail(form.as_p())
